b6aa16b7e6e04743d57aac54169d7b2cf9c03aa2
Tuomas Männistö <tuomas.o.mannisto@gmail.com>
2019-07-31 20:11:46 +0300

    puhelinluettelo full stack 3.11 done

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
-app.use(bodyParser.json())
+app.use(express.static('build'))
 app.use(cors())
+app.use(bodyParser.json())
 
 morgan.token('data', (req, res) => {
   return JSON.stringify(req.body)
